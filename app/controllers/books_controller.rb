e318c47744aece189f747375c56d11d3588eb0dd
032-shi <0324omitsu@gmail.com>
2021-04-12 10:15:49 +0000

    [update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,13 +22,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
